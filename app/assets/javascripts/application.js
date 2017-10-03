2b2cd918a9f5bda425e09d90599aa9bfe16dba6a
pecul <peculcommunity@gmail.com>
2017-10-03 10:45:59 +0530

    github API call successfull

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,44 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+var a;
+var valid_hack_array=[];
+function checkfromgithub(val) {
+	var html="";
+	valid_hack_array=[];
+	if(val.trim()==0){
+		//append something
+		return;
+	}
+	//hundredeir
+	else{
+		$.ajax({
+			url : "https://api.github.com/search/issues?q=author%3A"+val+"+type%3Apr",
+			dataType : "jsonp",
+			success : function ( returndata ) {
+				// console.log(returndata.data.items)
+				$.each( returndata.data.items, function ( i, item ) {
+					if( moment(returndata.data.items[i].created_at).isAfter(moment('01/10/2017','DD/MM/YYYY')) )
+						valid_hack_array.push(returndata.data.items[i]);
+				html+=	'<div class="col-sm-12">'+returndata.data.items[i].created_at+'</div>'+'<br>'
+				});
+				$( '#result' ).html(html);
+						console.log(valid_hack_array);
+			}
+		});
+	}
+}
+
+//function get_watched_repos() 
+//{
+//   
+//  $( '#result' ).html( "" );
+//  var html = "<h2>Repos I'm watching</h2>";
+//  
+  
+//  
+//}
+
+//}); //close document ready
